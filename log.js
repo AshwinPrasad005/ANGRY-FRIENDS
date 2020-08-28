b9bd8dc93ffc1e93b1faeb032310a5cbc6fd633f
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-08-28 19:38:04 +0530

    Add files via upload

--- log.js
+++ log.js
@@ -1,3 +1,24 @@
 class Log{
-    
+    constructor(x,y,width,height){
+        var options={
+            isStatic:false
+        }
+        this.image = loadImage("log.png");
+        this.body = Bodies.rectangle(x,y,width,height);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        fill("brown");
+        noStroke();
+        image(this.image,0,0,this.width,this.height);
+        pop();
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var engine,world,ground,box1,box2;
+var engine,world,ground,box1,box2,log1,box3,box4,log2;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -19,7 +19,12 @@ function setup() {
 
 	ground = new Ground(300,595,600,10);
 	box1 = new Box(400,500,50,50);
-	box1 = new Box(500,500,50,50);
+	box2 = new Box(500,500,50,50);
+	log1 = new Log(450,450,150,10);
+
+	box3 = new Box(400,400,50,50);
+	box4 = new Box(500,400,50,50);
+	log2 = new Log(450,390,150,10)
   
 }
 
@@ -30,6 +35,11 @@ function draw() {
   ground.display();
   box1.display();
   box2.display();
+  log1.display();
+
+  box3.display();
+  box4.display();
+  log2.display();
   
   drawSprites();
  
