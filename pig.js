c2fd2993c63af322c0dfdf68b524f2e95dcfafb1
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-10-25 12:28:23 +0530

    Add files via upload

--- pig.js
+++ pig.js
@@ -10,10 +10,12 @@ class Pig{
         this.body = Bodies.rectangle(x,y,width,height,options);
         this.width = width;
         this.height = height;
+        this.Visiblity = 255;
         World.add(world,this.body);
     }
     display(){
-        var pos = this.body.position;
+        if(this.body.speed < 3){
+            var pos = this.body.position;
         var angle = this.body.angle;
         push();
         translate(pos.x,pos.y);
@@ -21,5 +23,12 @@ class Pig{
         imageMode(CENTER);
         image(this.image,0,0,this.width,this.height);
         pop();
+        }
+        else{
+            World.remove(world, this.body);
+            push();
+            this.Visiblity = this.Visiblity - 5;
+            tint(255,this.Visiblity);
+        }
     }
 }
